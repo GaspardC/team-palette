f1499ebd38975cba49053885a44e5ddbbc93019c
lueyam210 <mayeul.barthes@epfl.ch>
2016-04-08 13:27:22 +0200

    Added files via upload
    The website to test different algorithms once we'll get the pictures.

---
+++ Rating_website/error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Testing color changes</title>
+	</head>
+
+	<body>
+		<div id="mainwrapper">
+			<h2>Error!</h2>
+			
+			<p>
+			You are missing some crucial parameters, something has gone <em>WRONG!</em> 
+			</p>
+			<p>
+			You can try to start the form again <a href="index.php">here</a>.
+			</p>
+		</div>
+	</body>
+</html>	
